c889d39ce6f8dfc5a1698ef7231b6e873108d616
minemo <wittki56@gmail.com>
2024-04-03 13:36:57 +0200

    artifact publishing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::env::set_var;
+
 fn main() {
     // read env variables that were set in build script
     let uefi_path = env!("UEFI_PATH");
@@ -5,13 +7,24 @@ fn main() {
 
     println!("UEFI-Kernel at: {}\nBIOS-Kernel at: {}", uefi_path, bios_path);
     
-    let mut cmd = std::process::Command::new("qemu-system-x86_64");
-    cmd.arg("-drive").arg(format!("format=raw,file={bios_path}"));
+    match std::env::var_os("CI") {
+        Some(v) => {
+            println!("In CI environment, setting env-vars");
+            set_var("BUILD_UEFI_PATH", uefi_path);
+            set_var("BUILD_BIOS_PATH", bios_path);
+        },
+        _ => {
+            println!("Not in CI environment, running qemu");
+            let mut cmd = std::process::Command::new("qemu-system-x86_64");
+            cmd.arg("-drive").arg(format!("format=raw,file={bios_path}"));
+            
+            // add ISA debug OS exit
+            cmd.arg("-device").arg("isa-debug-exit,iobase=0xf4,iosize=0x04");
+            
+            // run qemu
+            let mut child = cmd.spawn().unwrap();
+            child.wait().unwrap();
+        }
+    }
     
-    // add ISA debug OS exit
-    cmd.arg("-device").arg("isa-debug-exit,iobase=0xf4,iosize=0x04");
-    
-    // run qemu
-    let mut child = cmd.spawn().unwrap();
-    child.wait().unwrap();
 }
