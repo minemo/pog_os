bef92b8d1f35b4a6aa49b60d82ca32e73fd9bd0d
minemo <wittki56@gmail.com>
2024-04-03 13:16:17 +0200

    use bios instead of uefi

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,7 @@ fn main() {
     println!("UEFI-Kernel at: {}\nBIOS-Kernel at: {}", uefi_path, bios_path);
     
     let mut cmd = std::process::Command::new("qemu-system-x86_64");
-    cmd.arg("-bios").arg(ovmf_prebuilt::ovmf_pure_efi());
-    cmd.arg("-drive").arg(format!("format=raw,file={uefi_path}"));
+    cmd.arg("-drive").arg(format!("format=raw,file={bios_path}"));
     
     // add ISA debug OS exit
     cmd.arg("-device").arg("isa-debug-exit,iobase=0xf4,iosize=0x04");
